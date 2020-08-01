fb7387902b6d2c6e19bf1ea69b4fa0a1fbf50118
Simone Rossi <simonerossi@protonmail.com>
2020-08-01 03:26:18 +0200

    Add index template with initial structure

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './templates');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', { title: 'Home' });
 });
 
 app.listen(3000, function () {
