75b523f528cddc915aaeba5adbe6bc18bbca512d
Simone Rossi <simonerossi@protonmail.com>
2020-08-03 13:49:32 +0200

    Send switch to server and store it in database

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ app.set('view engine', 'pug');
 app.set('views', './templates');
 
 app.use(express.static('static_files'));
+app.use(express.json());
 
 app.get('/', function (req, res) {
   db.getLastStatus(req.ip, function(lastStatus) {
     res.render('index', { title: 'Home', lastStatus: lastStatus });
   })
+});
+
+app.post('/switches', function(req, res) {
+  db.insertSwitch(req.ip, req.body.mode, function() { res.status(201).end(); });
 });
 
 app.listen(3000, function () {
--- static_files/script.js
+++ static_files/script.js
@@ -1,16 +1,27 @@
 function toggleSwitch () {
-    var switchContainerEl = document.getElementsByClassName('switch-container')[0];
-    var switchEl = document.getElementsByClassName('switch')[0];
-    var body = document.body;
+  var switchContainerEl = document.getElementsByClassName('switch-container')[0];
+  var switchEl = document.getElementsByClassName('switch')[0];
+  var body = document.body;
+  
+  if(switchContainerEl.getAttribute('class').includes('light')) {
+    sendSwitchToServer('dark');
+    switchContainerEl.setAttribute('class', 'switch-container dark');
+    switchEl.setAttribute('class', 'switch dark');
+    body.setAttribute('class', 'dark');
+  } else {
+    sendSwitchToServer('light');
+    switchContainerEl.setAttribute('class', 'switch-container light');
+    switchEl.setAttribute('class', 'switch light');
+    body.setAttribute('class', 'light');
+  }
+}
 
-    if(switchContainerEl.getAttribute('class').includes('light')) {
-        switchContainerEl.setAttribute('class', 'switch-container dark');
-        switchEl.setAttribute('class', 'switch dark');
-        body.setAttribute('class', 'dark');
-    } else {
-        switchContainerEl.setAttribute('class', 'switch-container light');
-        switchEl.setAttribute('class', 'switch light');
-        body.setAttribute('class', 'light');
-    }
-    
+function sendSwitchToServer(mode) {
+  fetch('/switches', { 
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ mode: mode }) 
+  }).catch(function() {
+    console.log('Error to post');
+  });
 }
